04c98da60de850a5465d7272f5a4d0004a173a4a
Thibaut <thibautstachnick@gmail.com>
2024-05-30 22:58:16 +0200

    add variables

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+fn is_palindrome(number: i32) {
+    let mut forward_numbers:i32;
+    let mut backward_numbers:i32;
+    for sub_number in number {
+        println!("{}",sub_number)
+    }
 }
+
+
+
+fn main() {
+    let x = 3628;
+    is_palindrome(x);
+}
+
